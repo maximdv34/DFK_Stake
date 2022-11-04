4ca7e2bd5e3c9cd5e1b575b3cdc94c44fe45ebdb
StaticReadonly <klymukvlad4002@gmail.com>
2022-11-04 19:40:29 +0300

    Upgraded Header_Top and Home components

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -20,16 +20,21 @@ function App() {
 
 function Header_Top() {
   return(
-    <>
-    <div>
-      <div>DFK Stake</div>
-      <div><img src='./static/images/logo.png'/></div>
-      <div>
-        <Link to={'Staking'}><div>Staking</div></Link>
-        <Link to={'AboutUs'}><div>About Us</div></Link>
-      </div>`
-    </div>
-    </>
+        <>
+          <header>
+              <div>
+                  <Link to={'/'}><div className="MainName">DFK Stake</div></Link>
+                  <Link to={'/'}><img src='./static/images/logo.png' /></Link>
+              </div>
+              <div className="Navigation">
+                  <Link to={'Staking'}><div>Staking</div></Link>
+                  <Link to={'AboutUs'}><div>About Us</div></Link>
+              </div>
+              <div className="ConnectWalletButton">
+                  <button>Connect Wallet</button>       
+              </div>
+            </header>
+        </>
     );
 }
 export default App;
--- ClientApp/src/Home.jsx
+++ ClientApp/src/Home.jsx
@@ -5,7 +5,27 @@ import './styles/Home.css';
 export default function Home() {
      return(
     <>
-    <div className="title-home">DFK Stake</div>
+             <main>
+                 <div className="HomeMain">
+                     DFK Stake 
+                     <img src='./static/images/logo.png' /> 
+                 </div>
+                 <div>
+                    <div>
+                         <h1>Used by millions. Trusted with billions.</h1>
+                         <p>DFK state has the most users of any decentralized platform ever.</p>
+                         <p>And those users are now entrusting the platfrom with over 10$ billion in funds</p>
+                    </div>
+                    <div>
+                         <h1>Earn passive income with crypto.</h1>
+                         <p>DFK State makes it easy to make your crypto work for you.</p>
+                         <div className="ButtonsContainer">
+                             <button>Explore</button>
+                             <button>Learn</button>
+                         </div>
+                     </div>
+                 </div>
+             </main>
     </>
     );
 }
