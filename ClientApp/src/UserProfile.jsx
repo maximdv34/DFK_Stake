ad28983f58aba95e54fcc5870defd541868077c2
StaticReadonly <klymukvlad4002@gmail.com>
2022-11-19 15:07:01 +0300

    usserproifle props added

--- ClientApp/src/UserProfile.jsx
+++ ClientApp/src/UserProfile.jsx
@@ -2,14 +2,19 @@ import './styles/UserProfile.css';
 import React, { useState} from 'react';
 
 export default function UserProfile(props) {
-    const [copiedToClipboard, setCopiedToClipboard] = useState(false);
-    const upwp = "0x0e7dbcEEEde7573D28ec8ec8ce2e3e8366355ce";   //user profile wallet placeholder
-    const upwpd = upwp.slice(0, 7) + "...." + upwp.slice(-4); //user profile wallet for display in profile
-    const balance = 12345;                                      //user wallet balance
+    //props.onDisconnect;
+    //props.balance
+    //props.account
 
+    const accountDisplay = props.account.slice(0, 7) + "...." + props.account.slice(-4);
+    //const upwp = "0x0e7dbcEEEde7573D28ec8ec8ce2e3e8366355ce";   //user profile wallet placeholder
+    //const upwpd = upwp.slice(0, 7) + "...." + upwp.slice(-4); //user profile wallet for display in profile
+    //const balance = 12345;                                      //user wallet balance
+
+    const [copiedToClipboard, setCopiedToClipboard] = useState(false);
     //Copy wallet id to clipboard
     const copyWalletIdToClipboard = async () => {
-        await navigator.clipboard.writeText(upwp);
+        await navigator.clipboard.writeText(props.account);
         setCopiedToClipboard(true);
         setTimeout(setCopiedToClipboard, 3000, false);
     };
@@ -27,7 +32,7 @@ export default function UserProfile(props) {
             <div className="UserProfileDataContainer">
                   <img src="./static/images/UserImages/WalletImage.png" alt="Wallet"/>
                   <div onClick={copyWalletIdToClipboard }>
-                        {upwpd}
+                        {accountDisplay}
                   </div>
                   </div>
                     {copiedToClipboard ? (
@@ -39,11 +44,11 @@ export default function UserProfile(props) {
 
             <div className="UserProfileDataContainer">
                 <div>
-                 {balance}$
+                 {props.balance}$
                 </div>
             </div>
             <div className="UserProfileDisconnectButton">
-                <button>Disconnect</button>
+                <button onClick={onDisconnect }>Disconnect</button>
             </div>
         </div>
         );
